85084913eed1204a1a9e647c17caf237aa0e38ad
Gena <gena@e64.net>
2017-01-26 23:25:33 +0100

    Récupération du temps à toulouse

---
+++ app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+	$('#recupererTemps').click(function(){
+		// faire la demande au serveur de temps
+		$.get( "http://api.openweathermap.org/data/2.5/forecast/city?id=2972315&APPID=b726b2e9f4f5440fc9c82f8d5ca6fecc", function(data){
+			console.log('YEAH il m a repondu', data);
+			$('#resultat').html('<h1>' + data.city.name + ' (' + data.city.country + ') (' + data.city.coord.lon + ', ' + data.city.coord.lat + ')</h1>' );
+			//$('#resultat').append("<h2>" + data.weather[0].description + "</h2>");
+			data.list.forEach(function(currentValue, index, tableau){
+				$('#resultat').append("<div>" + currentValue.dt_txt + " : " + Math.round((currentValue.main.temp  - 273.15)*10)/10 + "°C</div>");
+			});
+		} );
+	});
+});
